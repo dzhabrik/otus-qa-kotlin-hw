190ced833eb9dddf2d52dd044360b0fe9483d7fb
Iaroslav Khorkhordin <iaroslav.khorkhordin@abbyy.com>
2023-06-10 11:22:51 +0200

    init repo

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "org.otus"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    runtimeOnly("io.kotest:kotest-framework-api-jvm:5.6.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    val kotestVersion = "5.6.2"
+    implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
