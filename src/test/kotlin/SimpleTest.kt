93b824e20251571abfc9e0022b3b1172a8632b0e
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-06-10 16:27:15 +0300

    Задание 3 принято.

--- src/test/kotlin/SimpleTest.kt
+++ src/test/kotlin/SimpleTest.kt
@@ -3,13 +3,18 @@ import io.kotest.matchers.shouldBe
 
 class MyTests : FunSpec({
 
-    test("My test") {
-        println("Start test")
-        1 + 2 shouldBe 4
+    test("My test1 Passed") {
+        println("Start test1")
+        1 + 2 shouldBe 3
     }
 
-    test("My test1") {
-        println("Start test")
-        1 + 2 shouldBe 3
+    test("My test2 Failed") {
+        println("Start test2")
+        1 + 2 shouldBe 4
+    }
+    test("My test3 Exception") {
+        println("Start test3")
+        throw IllegalStateException("some exception thrown")
+        //1 + 2 shouldBe 4
     }
 })
