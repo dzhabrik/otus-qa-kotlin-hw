697781614796df77d3f037bdeca0d8911411dc95
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-06-12 16:41:14 +0300

    Задание 2

--- homework02/src/test/kotlin/TasksRepositoryTests.kt
+++ homework02/src/test/kotlin/TasksRepositoryTests.kt
@@ -2,58 +2,74 @@ import data.Priority
 import data.Task
 import data.TasksRepository
 import data.TasksRepositoryMemory
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.assertAll
-import kotlin.test.*
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import kotlin.test.assertContains
+import kotlin.test.assertContentEquals
+// используйте максимум или все из JUNIT5
+//import kotlin.test.*
+
 
 class TasksRepositoryTests {
 
     private lateinit var tasksRepository: TasksRepository
 
-    @BeforeTest
+
+    //@BeforeTest
+    @BeforeEach
     fun initTasksRepository() {
         tasksRepository = TasksRepositoryMemory()
-        assertTrue("Tasks were not added to list. Can't run test") {
-            (tasksRepository as TasksRepositoryMemory).tasks.run {
+        // это assert мало что проверяет и лучше не иметь assert в @BeforeTest
+       // assertTrue("Tasks were not added to list. Can't run test") {
+            with ((tasksRepository as TasksRepositoryMemory).tasks) {
+            // (tasksRepository as TasksRepositoryMemory).tasks.run {
                 add(Task(2, "task2", Priority.MEDIUM))
                 add(Task(1, "task1", Priority.LOW))
                 add(Task(3, "task3", Priority.MEDIUM))
                 add(Task(4, "task4", Priority.HIGH, true))
                 add(Task(5, "task5", Priority.LOW, true))
             }
-        }
+        //}
     }
 
-    @AfterTest
+    //@AfterTest
+    @AfterEach
     fun clearRepository() {
-        (tasksRepository as TasksRepositoryMemory).tasks.clear()
+        // @BeforeTest срабатывает перед КАЖДЫМ тестом и организует "свежий" репозиторий с пустой "базой"
+        // нет смысла чистить использованную базу после каждого теста и стек ее задач тоже
+       //(tasksRepository as TasksRepositoryMemory).tasks.clear()
     }
 
     @DisplayName("Check task adding to repo")
     @Test
     fun addTaskToListTest() {
-        val nextTaskId = (tasksRepository as TasksRepositoryMemory).tasks.size + 1
-        val task = Task(nextTaskId, "task${nextTaskId}", Priority.LOW)
+        val nextTaskId = (tasksRepository as TasksRepositoryMemory).nextId()
+        //val nextTaskId = (tasksRepository as TasksRepositoryMemory).tasks.size + 1
+        val task = Task(nextTaskId, "task$nextTaskId", Priority.LOW)
         tasksRepository.addTask(task)
-        assertContains(tasksRepository.getTasks(task.completed), task)
+        //assertContains(tasksRepository.getTasks(task.completed), task)
+        assertContains(tasksRepository.getTasks(), task)
     }
 
     @DisplayName("Check completed tasks filter")
-    @Test
+    @RepeatedTest(7)
     fun completedTasksFilterTest() {
-        val undoneTask = tasksRepository.getTasks().random()
+        val undoneTask = tasksRepository.getTasks().filter { !it.completed }.random()
         undoneTask.id?.let { tasksRepository.completeTask(it) }
-        val uncompletedTasks = tasksRepository.getTasks(true).filter { !it.completed }
-
+        //val uncompletedTasks = tasksRepository.getTasks(true).filter { !it.completed }
+        val uncompletedTasks = tasksRepository.getTasks(false)
         assertAll(
             { assertTrue(undoneTask.completed) },
-            { assertEquals(0, uncompletedTasks.size, "Filter does not exclude uncompleted tasks") }
+            { assertEquals(2, uncompletedTasks.size, "Filter does not exclude uncompleted tasks") }
         )
     }
 
     @DisplayName("Check filter by name and priority")
+    @Disabled // см CHECKME.md
     @Test
     fun nameAndPriorityFilterTest() {
+        // undoneTasks и completedTasks будут одинаковые - там true параметр по умолчанию
         val undoneTasks = tasksRepository.getTasks()
         val completedTasks = tasksRepository.getTasks(true)
 
